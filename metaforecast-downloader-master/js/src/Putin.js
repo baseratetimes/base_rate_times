e4da517a708c23fac46f81068b7d1c5579b461a4
baseratetimes <marcelvandiemen@protonmail.com>
2023-07-01 13:19:26 +1000

    Modify again again

--- metaforecast-downloader-master/js/src/Putin.js
+++ metaforecast-downloader-master/js/src/Putin.js
@@ -33,43 +33,53 @@ import axios from 'axios';
 import { Parser } from 'json2csv';
 import fs from 'fs';
 
-// Define the GraphQL query.
-const query = `
-  {
-    question(id: "metaculus-10084") {
-      history {
-        options {
-          name
-          probability
+// Define the ids for which we want to fetch data.
+const ids = ["goodjudgmentopen-2617", "infer-1263", "metaculus-13930", "polymarket-0x9de1bbb5", "manifold-LZuynBJB6zTiKm0HZuDK", "insight-192967"];
+
+// Initialize an array to hold all time series data.
+let allTimeSeriesData = [];
+
+// Define a function to fetch data for a given id.
+async function fetchDataForId(id) {
+  // Define the GraphQL query.
+  const query = `
+    {
+      question(id: "${id}") {
+        history {
+          options {
+            name
+            probability
+          }
+          fetchedStr
         }
-        fetchedStr
       }
     }
-  }
-`;
+  `;
 
-// Send a POST request to the GraphQL endpoint.
-axios({
-  url: 'https://metaforecast.org/api/graphql',
-  method: 'post',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  data: {
-    query: query
-  }
-}).then((response) => {
+  // Send a POST request to the GraphQL endpoint.
+  const response = await axios({
+    url: 'https://metaforecast.org/api/graphql',
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      query: query
+    }
+  });
+
   // Check if the request was successful.
   if (response.status !== 200) {
     throw new Error('GraphQL query failed');
   }
 
-  // Extract the time series data.
+  // Extract the time series data for this id.
   let timeSeriesData = [];
   for (let historyItem of response.data.data.question.history) {
     for (let option of historyItem.options) {
       if (option.name === 'Yes') {
         timeSeriesData.push({
+          id: id,
           time: historyItem.fetchedStr,
           probability: option.probability
         });
@@ -77,12 +87,19 @@ axios({
     }
   }
 
+  // Add the time series data for this id to the overall data.
+  allTimeSeriesData = allTimeSeriesData.concat(timeSeriesData);
+}
+
+// Fetch data for all ids.
+Promise.all(ids.map(fetchDataForId)).then(() => {
   // Convert the time series data to CSV.
   const json2csvParser = new Parser();
-  const csv = json2csvParser.parse(timeSeriesData);
+  const csv = json2csvParser.parse(allTimeSeriesData);
 
   // Write the CSV data to a file.
-  fs.writeFileSync('metaculus-10084.csv', csv);
+  fs.writeFileSync('allData.csv', csv);
 }).catch((error) => {
   console.error(error);
 });
+
