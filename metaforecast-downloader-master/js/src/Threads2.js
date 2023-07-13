99995d486fbb7bc48d415de14b3f0dbcc4442fab
baseratetimes <marcelvandiemen@protonmail.com>
2023-07-13 18:06:49 +1000

    130723 606

--- metaforecast-downloader-master/js/src/Threads2.js
+++ metaforecast-downloader-master/js/src/Threads2.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 import retry from 'async-retry';
 
 // Define the ids for which we want to fetch data.
-const ids = ["manifold-FxpNqBVG6NIvmZFVqG1P", "manifold-JxcpJTGSurSdSMlFxYhL", "metaculus-14259", "manifold-qqFkvYHzzpTuonxnBWpN", "polymarket-0x5284c2e5"];
+const ids = ["polymarket-0xea5562d9", "metaculus-14258", "metaculus-14257", "manifold-SttdxBP4Edxqzq0ScDCH", "goodjudgmentopen-2631"];
 
 // Initialize an array to hold all time series data.
 let allTimeSeriesData = [];
